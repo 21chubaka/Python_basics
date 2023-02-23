3368cb5c981563a9260c06c12b862d3f8e3e3131
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-02-23 14:35:53 +0000

    2023.02.23: Currency Exchange prog

--- currency_exchange.py
+++ currency_exchange.py
@@ -3,3 +3,12 @@ Currency Exchange:
 Program to take one currency as a float and converts it to another currency as a float
 For this example will be using an amount in US Dollars to be converted into Canadian Dollars
 """
+
+# US Dollar amount:
+us_amt = 212005.03
+
+# Canadian Dollar to US Dollar rate:
+can_dol_rate = 1.26
+
+print('Amount in US Dollars: ', us_amt)
+print('Amount in Canadian Dollars: ', us_amt * can_dol_rate)
