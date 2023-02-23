7c65c405ecccf501de131e119dca28ecb0b614ee
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-02-23 14:28:58 +0000

    2023.02.23: Curr Exchg file

---
+++ currency_exchange.py
@@ -0,0 +1,5 @@
+"""
+Currency Exchange:
+Program to take one currency as a float and converts it to another currency as a float
+For this example will be using an amount in US Dollars to be converted into Canadian Dollars
+"""
