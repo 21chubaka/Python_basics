4c14945c01ff07ffa9ac21e1d5b6a177f90a9232
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-02-27 17:39:32 +0000

    2023.02.27: City_Province prog draft

---
+++ 3_city_province.py
@@ -0,0 +1,4 @@
+"""
+A program that takes a user's input of a town/city from a small amount of town/cities from Ireland and
+return what Province the town/city is located in if there is a match.
+"""
