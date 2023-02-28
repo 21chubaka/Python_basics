aa953b5ee39d79b70a0087009cd7eab7e53b9f56
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-02-28 16:38:41 +0000

    2023.02.28: Password prog

--- 4_password.py
+++ 4_password.py
@@ -10,3 +10,19 @@ inputs match the password. If so, notify user they have successfully
 logged in. If not notify they have been denied access.
 
 """
+password = 'PythonRocks!'
+
+user_input = input('Please enter your password: ')
+
+if user_input == password:
+    print('You have successfully logged in.')
+
+else:
+    print('You have entered an incorrect password. Please enter your password three times.')
+    first_user_inpt = input('Please enter your password: ')
+    sec_user_inpt = input('Please enter your password: ')
+    third_user_inpt = input('Please enter your password: ')
+    if first_user_inpt == password and sec_user_inpt == password and third_user_inpt == password:
+        print('You have successfully logged in.')
+    else:
+        print('You have been denied access.')
