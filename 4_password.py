3a6ad6b4e5a24b64465d955bd064d6cc416c129f
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-02-28 16:35:56 +0000

    2023.02.28: Password program draft

---
+++ 4_password.py
@@ -0,0 +1,12 @@
+"""
+Password Checking 3 times program:
+
+Assign a password to password variable.
+Prompt a user to enter their password.
+Check if the user input matches the password, if so tell the user they
+have successfully logged in. Else notify the user the password if wrong and
+prompt the user to enter their password three times. Check if all three
+inputs match the password. If so, notify user they have successfully
+logged in. If not notify they have been denied access.
+
+"""
