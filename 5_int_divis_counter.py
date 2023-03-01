d95cacf58d765c6568a376c38236d0396fd9d842
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-01 10:40:33 +0000

    2023.03.01: Sum of Int divis by 3 or 5 file

---
+++ 5_int_divis_counter.py
@@ -0,0 +1,10 @@
+'''
+Sum of 1-10000; divisble by 3 or 5 program:
+
+Create a counter variable and set it to 10000. Create a total variable
+and set it to 0. Create a while loop that continues until it is zero.
+Inside the while loop check if the counter is divisble by 3 or 5. If so,
+increment the total variable by counter then decrement counter by one. If not,
+just decrement the counter by one. Outside the while loop print the sum of the
+integers divisble by 3 or 5 in the range of 1-10000 using the total variable.
+'''
