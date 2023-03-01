9216967d59afa02351bb4627867902b87a2d17c4
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-01 10:41:59 +0000

    2023.03.01: Int divis working prog

--- 5_int_divis_counter.py
+++ 5_int_divis_counter.py
@@ -8,3 +8,15 @@ increment the total variable by counter then decrement counter by one. If not,
 just decrement the counter by one. Outside the while loop print the sum of the
 integers divisble by 3 or 5 in the range of 1-10000 using the total variable.
 '''
+
+counter = 10000
+total = 0
+
+while counter != 0:
+    if counter % 3 == 0 or counter % 5 == 0:
+        total += counter
+        counter -= 1
+    else:
+        counter -= 1
+
+print('The sum of integers divisble by 3 or 5 in the range of 1-10,000:', total)
