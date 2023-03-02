f4c6a2da5a9c4ef061a31359127aa1bbf4e13229
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-02 12:22:56 +0000

    2023.03.02: Pseudocode

---
+++ 6_int_series_range_count.py
@@ -0,0 +1,11 @@
+'''
+Number Range Counter program:
+Intialize user_input variable and series of number range count variables; all set to zero.
+Inform user of rules of program.
+While user_input is greater or equal to zero.
+    Prompt user to enter number.
+    Check if the user_input is negative, if so print out break out of while and print the analysis.
+    Set up series of if checks for the number ranges. If number is in range increment that range counter by one.
+        and inform user if it is in that range.
+End while ends or breaks, print analysis statements
+'''
