01bedf7e16f414148dfffa23d730a6dcff38bf4a
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-03 09:47:44 +0000

    2023.03.03: Pizza coombo Pseudocode

---
+++ 7_pizza_combos.py
@@ -0,0 +1,12 @@
+'''
+Pizza Topping Combos program:
+Prompt the user for number of toppings set it to num_top.
+Prompt the user for number of toppings on a standard pizza, set it to stand_top.
+Intialize variables n_fac and k_fac and set to one.
+Intialize n_k_fac and set it to num_top - stand_top
+Check it num_top, stand_top, and n_k_fac are greater or equal to zero.
+    set up three factorial loops for n, k, and (n-k)
+Else, negative number inform user
+Intialize variable combos and set to n_fac / (k_fac * n_k_fac)
+Return results to user
+'''
