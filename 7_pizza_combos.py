a8cb3cf4e0454fab56bb552331a7200d78f796e8
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-03 10:07:19 +0000

    2023.03.03: Pizza Combo draft

--- 7_pizza_combos.py
+++ 7_pizza_combos.py
@@ -10,3 +10,23 @@ Else, negative number inform user
 Intialize variable combos and set to n_fac / (k_fac * n_k_fac)
 Return results to user
 '''
+num_top = int(input('Number of toppings today: '))
+stand_top = int(input('Number of toppings on Standard Pizza today: '))
+
+n_fac = 1
+k_fac = 1
+n_k_fac = num_top - stand_top
+
+if num_top >= 0 and stand_top >= 0 and n_k_fac >= 0:
+    for i in range(1, num_top + 1):
+        n_fac = num_top * i
+    for j in range(1, stand_top + 1):
+        k_fac = stand_top * j
+    for k in range(1, n_k_fac + 1):
+        n_k_fac = n_k_fac * k
+else:
+    print('That is a negative number. Please try again')
+
+combos = n_fac / (k_fac * n_k_fac)
+
+print('Number of combinations: ', combos)
