af93aec3462325a0ef72b9127ff05a666fc76ea0
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-06 18:09:17 +0000

    2023.03.06: Pseudocode vowel counter

---
+++ 8_vowel_counter.py
@@ -0,0 +1,14 @@
+"""
+Vowel Counter:
+Infrom user of the rules of the program
+Prompt the user for a string
+Update user input string to lowercase
+Intialise vowel counter variables
+Check if the user's input is not empty:
+    for loop to iterate through user input:
+        Check if the current letter is a vowel:
+            Increment correct vowel counter variable
+    Return counts of vowels to the user
+Else:
+    Exit program
+"""
