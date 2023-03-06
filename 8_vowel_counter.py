070fbf54bd23ea61c53c442cf676106ecfd3b31b
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-06 18:12:29 +0000

    2023.03.06: Working Vowel Counter

--- 8_vowel_counter.py
+++ 8_vowel_counter.py
@@ -12,3 +12,35 @@ Check if the user's input is not empty:
 Else:
     Exit program
 """
+
+print('Provide a string and I will tell you how many vowels there is.')
+print('Enter an empty string to exit the program.')
+user_input = input('Please enter a string: ')
+
+low_user_input = user_input.lower()
+count_a = 0
+count_e = 0
+count_i = 0
+count_o = 0
+count_u = 0
+
+if user_input != "":
+    for letter in low_user_input:
+        if letter == 'a':
+            count_a += 1
+        if letter == 'e':
+            count_e += 1
+        if letter == 'i':
+            count_i += 1
+        if letter == 'o':
+            count_o += 1
+        if letter == 'u':
+            count_u += 1
+
+    print('Number of a\'s: ', count_a)
+    print('Number of e\'s: ', count_e)
+    print('Number of i\'s: ', count_i)
+    print('Number of o\'s: ', count_o)
+    print('Number of u\'s: ', count_u)
+else:
+    print('Empty string. Goodbye.')
