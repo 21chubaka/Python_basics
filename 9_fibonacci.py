d3119fb4930c939d38baeb48034653756ac7ee3f
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-07 19:40:14 +0000

    2023.03.07: Refactoring

--- 9_fibonacci.py
+++ 9_fibonacci.py
@@ -14,20 +14,26 @@ Else, if user_input equals one:
         Update b to c
         Increment count by one
 '''
+# Prompt the user for a positive integer
 user_input = int(input('Please enter a positive integer: '))
 
+# Program variable for Fib series
 a = 0
 b = 1
 count = 2
 
+# Check if user input is greater or equal to zero
 if user_input <= 0:
+    # Inform user and exit
     print("Enter a positive integer. Please try again.")
 else:
+    # Check if user input is equal to one
     if user_input == 1:
         print(a)
     else:
         print(a)
         print(b)
+        # While loop for fib series, until count is less than user input
         while count < user_input:
             c = a + b
             print(c)
