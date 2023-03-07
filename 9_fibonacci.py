1bd5c05d2a262b4cb117c334c5284b8a46294829
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-07 19:02:09 +0000

    2023.03.07: Pseudocode for Fib Series

---
+++ 9_fibonacci.py
@@ -0,0 +1,16 @@
+'''
+Fibonacci Series program:
+Prompt user to provide a positive integer
+Intialize three variables (0, 1, 2)
+Check if user_input is less than or equal to zero:
+    If so, Inform user to enter pos integer
+Else, if user_input equals one:
+    Return a
+    Else, print a & b
+    While count is less than user_input:
+        Update c to a + b
+        Return c
+        Update a to b
+        Update b to c
+        Increment count by one
+'''
