023f74dbcf922f220d8543201f7c18f097f16338
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-08 19:21:11 +0000

    2023.03.08: Working Aprox Sq Root

--- 10_aprox_sq_root.py
+++ 10_aprox_sq_root.py
@@ -11,3 +11,19 @@ Check if number and tolerance is greater than or equal to zero:
     If so, run function
     Else, inform user number & tolerance needs to be positive
 '''
+import sys
+
+def squareRoot(num, tol):
+    for i in range(0, int(num) + 1):
+        if (i ** 2 >= (num - tol)) and (i ** 2 <= (num + tol)):
+            print('The number', i, 'is the approximate square.')
+            sys.exit()
+
+print('I will ask for a positive number and positive tolerance number and return an approximation square root.')
+num = float(input('Please provide a positvie number: '))
+tol= float(input('Please provide a positvie tolerance number: '))
+
+if num >= 0 and tol >= 0:
+    squareRoot(num, tol)
+else:
+    print('Number and Tolerance must be a positive number. Please try again.')
