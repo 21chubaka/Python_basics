58554c6caa72d82eb914e064eb093610f5ba630f
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-08 17:53:57 +0000

    2023.03.08: Pseudocode for aprox sq root

---
+++ 10_aprox_sq_root.py
@@ -0,0 +1,13 @@
+'''
+Approximate Square Root program:
+Define a function that takes two arguments: number & tolerance
+    Inside function for i in range zero to number plus one
+        Check if i**2 is inside the tolerance range
+            If so, return the aprox square
+            Exit program
+Inform user of rules of program
+Ask user for number and tolerance
+Check if number and tolerance is greater than or equal to zero:
+    If so, run function
+    Else, inform user number & tolerance needs to be positive
+'''
