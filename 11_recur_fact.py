ea2a2a4d5df4ed23941b501a5c2421e7a3d9506f
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-09 18:29:31 +0000

    2023.03.09: Pseudocode Recur Fact

---
+++ 11_recur_fact.py
@@ -0,0 +1,14 @@
+'''
+Recursive Factorial program:
+Define a function that runs the factorial of an argument.
+    Check is argument is equal to zero or one, if so
+        set result variable to one
+        print argument and result
+    Else, set result to factorial equation
+        print argument and result
+Inform user of rules of program
+Prompt user for positive integer
+Check is user input is greater or equal to zero, if so:
+    Run function and print result
+Else, inform user number needs to be positive
+'''
