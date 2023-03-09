6db7f601a8662118460fe6d4b84a4aadeb40ed18
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-09 18:45:48 +0000

    2023.03.09: Refactoring

--- 11_recur_fact.py
+++ 11_recur_fact.py
@@ -12,6 +12,8 @@ Check is user input is greater or equal to zero, if so:
     Run function and print result
 Else, inform user number needs to be positive
 '''
+
+# factorial: parameter is a positvie integer
 def factorial(num):
     ''' Function that return the Factorial of its argument '''
     if num == 0 or num == 1:
@@ -23,10 +25,15 @@ def factorial(num):
 
     return result
 
+# Inform the user of the rules of the program
 print('Provide a positive integer and I will tell you its factorial.')
+
+# Prompt user for positive integer
 user_input = int(input('Provide a positive integer: '))
 
+# Check if the user's input is positive
 if user_input >= 0:
+    # Return factorial and cases using factorial()
     print('The factorial of', user_input, 'is', factorial(user_input))
 else:
     print('Number must be a positive integer. Please try again.')
