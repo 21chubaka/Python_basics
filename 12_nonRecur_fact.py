78b39a160bb5bea8b68bcb7132e96b7a944a7ba1
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-10 10:29:49 +0000

    2023.03.10: Pseudocode nonRecur Fact

---
+++ 12_nonRecur_fact.py
@@ -0,0 +1,14 @@
+'''
+Non-Recursive Factorial Time Track program:
+Import time tracking libs
+Intialize user_input variable and set it to zero.
+Intialize a total variable and set it to one.
+Inform the user of the rules of the program.
+While user_input is greater or equal to zero:
+    Prompt the user to enter a positive integer
+    For i in the range of one to user_input + 1
+        Update total to total * i
+    Return the factorial to the user
+    Reset total to one
+Else, inform the user it is a negative number
+'''
