247cab7e5f5c8e2009b2bda0f3764b1da4c068cd
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-03-10 10:38:21 +0000

    2023.03.10: Working NonRecur Fact

--- 12_nonRecur_fact.py
+++ 12_nonRecur_fact.py
@@ -12,3 +12,22 @@ While user_input is greater or equal to zero:
     Reset total to one
 Else, inform the user it is a negative number
 '''
+from timeit import default_timer as timer
+from datetime import timedelta
+
+start = timer()
+
+user_input = 0
+total = 1
+
+print('I will keep asking for positive interger numbers and telling you the Factorial of your number.')
+print('A negative number will end the program.')
+
+user_input = int(input('Please enter a positive number: '))
+for i in range(1, user_input + 1):
+    total = total * i
+print('The Factorial of', user_input, 'is', total)
+total = 1
+
+end = timer()
+print(timedelta(seconds=end-start))
