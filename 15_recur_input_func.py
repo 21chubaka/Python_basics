1fd4bcfb648cf3d8859b12d1c8efefb151551d10
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-01 21:14:16 +0100

    2023.08.01: Pseudocode

---
+++ 15_recur_input_func.py
@@ -0,0 +1,15 @@
+'''
+P15P3 - Third Recursive Function program:
+Define a function:
+    n == 0: 13
+    n == 1: 8
+    n < 0: exit
+    else: f(n - 2) + 13 * f(n - 1)
+
+Inform user of program rules
+Intialize user_input variable
+While user_input is greater or equal to 1:
+    Return function result
+    Prompt user for number
+Else, end program
+'''
