b34924d14981143ae6acabc422e5e524ff28c633
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-04 22:36:25 +0100

    2023.08.04: Pseudocode

---
+++ 16_common_divis_prog.py
@@ -0,0 +1,15 @@
+'''
+Common Divisors Function program:
+Implement the program to calculate common divisors from Lecture 16. You may use the solution on
+Pages 14–17 of the slides in your solution
+
+From Lecture 16 slides pages 16 & 17
+Define a function that takes two arguments
+    Runs through the range of one to the min of the two arguments:
+        If both are divisible by i add to a divisors tuple
+        Return divisors
+Prompt user for two ints
+Check if two user numbers are less than or equal to zero
+    If so, inform user number should be greater than zero
+Else, runs numbers into divisor function and print results
+'''
