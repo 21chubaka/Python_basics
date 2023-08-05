9b3de39d974827e67a0dd22e651fd6d8db3c75f8
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-05 22:04:07 +0100

    2023.08.05: Pseudocode

---
+++ 17_perfect_num.py
@@ -0,0 +1,19 @@
+'''
+Perfect Numbers Function program:
+Write a program that prompts the user for a positive integer and finds all the perfect numbers up to
+and including that number
+
+Define a function that finds all perfect numbers up to & including
+an arguent:
+    For i in range one to an argument plus one
+        Intialize a total variable and set it to zero
+        For j in range one to i
+            Check if i is divisble by j:
+                If so, increment total by j
+        Check if i equals total then return i
+Inform the user of the rules of the program
+Prompt the user for the positive integer
+Check if user input is less than or equal to zero:
+    if so, inform the user
+Else, run the user input into the Perfect Num function
+'''
