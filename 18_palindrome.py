fdc71106c47c9315163f155e64d95f17e271dd8e
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-06 13:37:10 +0100

    2023.08.06: Pseudocode

---
+++ 18_palindrome.py
@@ -0,0 +1,13 @@
+'''
+Palindrome Function program:
+From Lecture 17 Slides 19 & 20 with update from Ques P17P5
+Define a function that removes non-letters & turns a string to all lowercase
+Define a function to tell if a string is a Palindrome
+    Returns True if s is a palindrome;
+    Returns False otherwise.
+    Recursive func
+    Has print statements to trace its operation
+Prompts a user for a string
+While string is not empty, runs string into both functions
+If empty, program ends
+'''
