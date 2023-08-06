1c21d83bcec231da32ffeef3cd5a8e069a45c07a
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-06 13:39:57 +0100

    2023.08.06: Is it a Palindrome func

--- 18_palindrome.py
+++ 18_palindrome.py
@@ -23,3 +23,20 @@ def toChars(s):
         if c in 'abcdefghijklmnopqrstuvwxyz':
             letterstring += c
     return letterstring
+
+def isPal(s):
+    ''' Checks whether the string s is a palindrome
+
+    Assumes that s is a str with only lowercase letters & no non-letters.
+    Returns True if s is a palindrome;
+    Returns False otherwise.
+    Recursive func
+    Has print statements to trace its operation '''
+    print('isPal func called with argument', s)
+    if len(s) <= 1:
+        print('About to return True from isPal from the base case')
+        return True
+    else:
+        result = s[0] == s[-1] and isPal(s[1:-1])
+        print('About to return result', result, 'from isPal with argument', s)
+        return result
