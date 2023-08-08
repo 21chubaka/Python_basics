2bb40ddf1574167479b3b2026c43ab064764aa7b
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-08 21:28:17 +0100

    2023.08.08: Pseudocode

---
+++ 20_webPage_bracket_counter.py
@@ -0,0 +1,20 @@
+'''
+Backet Balance program:
+ Write a program that reads a file and checks that brackets (ie ( and ), < and >, { and }
+and [ and ]) are balanced, ie there should never be a situation where there are more right
+brackets of a particular type than there are corresponding left brackets and the total number
+of right brackets should equal the number of left brackets (You do not need to consider the
+interleaving of different bracket types). Your program should return the total number of each
+bracket and a message indicating whether or not the file has balanced brackets.
+
+Import os
+Check if the file does not exist
+    If so, inform the user
+Else
+    Read the file into a string variable
+    Set up counter variables
+    Set up total counter variables
+    Check if each of the barackets are balanced
+        If so, print results and inform which brackets are and are not balanced
+        Inform user if the total brackets page is balanced or not
+'''
