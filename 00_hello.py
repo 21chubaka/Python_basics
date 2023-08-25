f629196cb1d65d90b6e6e963f113a72f7474787c
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-25 21:24:05 +0100

    2023.08.25: Refactoring

--- 00_hello.py
+++ 00_hello.py
@@ -3,6 +3,8 @@ Hello World:
 Assign a concatenated variable of hello and world
 Print out the variable
 """
+# Greeting Variable
 greeting = 'Hello, ' + 'world.'
 
+# Print Greeting to user
 print(greeting)
