b890466444ce6c4ced918da2b1e344a95ce68ce3
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-25 21:35:48 +0100

    2023.08.25: Refactoring

--- 03_city_province.py
+++ 03_city_province.py
@@ -2,9 +2,13 @@
 A program that takes a user's input of a town/city from a small amount of town/cities from Ireland and
 return what Province the town/city is located in if there is a match.
 """
+# Prompt user for a Town/City input
 user_input = input('Enter a Town or City(string): ')
+
+# Convert user input to lowercase
 user_input_low = user_input.lower()
 
+# Check if user's input matches and of our towns/cities
 if user_input_low == 'dublin':
     print('You entered', user_input, '.', user_input, 'is in Leinster.')
 elif user_input_low == 'belfast':
