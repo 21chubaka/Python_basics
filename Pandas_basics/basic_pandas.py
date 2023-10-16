2bf28af1a83ac5b0b6d84bd160714f681ef8e9b0
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-10-16 18:32:37 +0100

    2023.10.16: Refactoring

--- Pandas_basics/basic_pandas.py
+++ Pandas_basics/basic_pandas.py
@@ -2,4 +2,6 @@ import pandas as pd
 
 # Simple Data Selection Function
 def selectData(data: pd.DataFrame) -> pd.DataFrame:
+    ''' This function takes a dataframe argument and returns
+        the selected student's name and age '''
     return data[data.student_id == 101][['name', 'age']]
