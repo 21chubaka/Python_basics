9df9d188938bd11b9d55dfe5452f7d1358a73739
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-10-16 21:47:34 +0100

    2023.10.16: Refactoring

--- Pandas_basics/basic_pandas.py
+++ Pandas_basics/basic_pandas.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 # Simple Data Selection Function
-def selectData(data: pd.DataFrame) -> pd.DataFrame:
+def selectData(data: pd.DataFrame):
     ''' This function takes a dataframe as an argument 
     and returns the selected student's name and age '''
     return data[data.student_id == 101][['name', 'age']]
 
 # Return the Size of a Dataframe
-def getDataFrameSize(df: pd.DataFrame) -> List[int]:
+def getDataFrameSize(df: pd.DataFrame):
     ''' This function takes a dataframe as an argument 
     and returns the size of the dataframe as a list '''
     return list(df.shape)
