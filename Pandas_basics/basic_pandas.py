265dc09893a9ccec1ae4e26d9f60b5518b679e0f
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-10-16 21:45:05 +0100

    2023.10.16: Dataframe size function

--- Pandas_basics/basic_pandas.py
+++ Pandas_basics/basic_pandas.py
@@ -5,3 +5,7 @@ def selectData(data: pd.DataFrame) -> pd.DataFrame:
     ''' This function takes a dataframe argument and returns
         the selected student's name and age '''
     return data[data.student_id == 101][['name', 'age']]
+
+# Return the Size of a Dataframe
+def getDataFrameSize(df: pd.DataFrame) -> List[int]:
+    return list(df.shape)
