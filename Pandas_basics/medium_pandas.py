45901ca818821568ca84aaeb798ef5f615000b22
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-10-19 19:24:58 +0100

    2023.10.19: Refactoring

--- Pandas_basics/medium_pandas.py
+++ Pandas_basics/medium_pandas.py
@@ -4,10 +4,14 @@ import pandas as pd
 def bigCountries(df):
     '''This function takes a dataframe as an argument,
     filters the dataframe to rows with 'area' greater than or equal to 3,000,000
-    AND 'population greater than or equal to 25,000,000, and returns the 'name',
+    AND 'population' greater than or equal to 25,000,000, and returns the 'name',
     'population', and 'area' fields of the filtered dataframe'''
     return df[(df.area >= 3000000) | (df.population >= 25000000)][['name', 'population', 'area']]
 
 # Return Products that are Low-Fat and Recyclable
 def findProducts(df):
+    '''This function takes a dataframe as an argument,
+    filters the dataframe to rows with 'low_fats' equal to 'Y'
+    AND 'recyclable' equal to 'Y', and returns the 'product_id' field of 
+    the filtered dataframe'''
     return df[(df.low_fats == 'Y') & (df.recyclable == 'Y')][['product_id']]
