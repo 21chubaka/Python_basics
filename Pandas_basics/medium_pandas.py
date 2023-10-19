edceb94b2c093eca651e3cfd713920f250772943
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-10-19 19:20:20 +0100

    2023.10.19: Refactoring

--- Pandas_basics/medium_pandas.py
+++ Pandas_basics/medium_pandas.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 # Return Big Countries
 def bigCountries(df):
+    '''This function takes a dataframe as an argument,
+    filters the dataframe to rows with 'area' greater than or equal to 3,000,000
+    AND 'population greater than or equal to 25,000,000, and returns the 'name',
+    'population', and 'area' fields of the filtered dataframe'''
     return df[(df.area >= 3000000) | (df.population >= 25000000)][['name', 'population', 'area']]
