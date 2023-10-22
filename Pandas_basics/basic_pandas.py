00ce36c6546624ec4500a8276abc9c0649103734
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-10-22 13:21:00 +0100

    2023.10.22: Cleanup

--- Pandas_basics/basic_pandas.py
+++ Pandas_basics/basic_pandas.py
@@ -2,14 +2,18 @@ import pandas as pd
 
 # Simple Data Selection Function
 def selectData(data: pd.DataFrame):
-    ''' This function takes a dataframe as an argument 
-    and returns the selected student's name and age '''
+    '''
+    This function takes a dataframe as an argument 
+    and returns the selected student's name and age
+    '''
     return data[data.student_id == 101][['name', 'age']]
 
 # Return the Size of a Dataframe
 def getDataFrameSize(df: pd.DataFrame):
-    ''' This function takes a dataframe as an argument 
-    and returns the size of the dataframe as a list '''
+    '''
+    This function takes a dataframe as an argument 
+    and returns the size of the dataframe as a list
+    '''
     return list(df.shape)
 
 # Return the 1st Three Rows
